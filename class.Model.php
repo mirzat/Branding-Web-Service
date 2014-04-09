d9548404d0f00b7802d70146d5742e6b10a452d7
Tashfique Mirza <tashfique.mirza@gmail.com>
2014-04-08 21:44:43 -0500

    Completed class functions in Model
    Updated functions in class.Model.php

--- class.Model.php
+++ class.Model.php
@@ -9,35 +9,66 @@
 ********************************************************************************/
 
 
-class Model
-	{
+class Model{
+		
 		// Declare Variables
-		public $metaData;  
+		public $metaData;
+		private $dbh;   
 
 	    public function __construct($url) {
-	    	include_once 'config.php';
+	    	include_once 'config.php';			// Get Database Connection Parameters
 	    			    	
-
+	    	$this->dbh = $this->createConnection();		// Connect to Database
+	    	$this->metaData = $this->getDataFromDB($url);	// Obtain the array containing the url, description and logo from db
+	    	$this->dbh = $this->closeConnection();		// Close connection to Database
 	    }
 
 	   
 	    // Creating database connection 
-	    private static function createConnection(){
-	    
+	    private static function createConnection()
+	    {
+	    	// Create an instance of the PDO class
+	    	try {
+	    			$dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD, 
+	    					array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	    		} 
+	    	
+	    	// Throw any exceptions
+	    	catch (PDOException $e) {
+	    		print "Error!: " . $e->getMessage() . "<br/>";
+	    		die();
+	    	}
+	    	
+	    	return $dbh;	
 		}
 		
 
 		// Function to get data from table
 		private function getDataFromDB($url){
 				
-
+			// To search by part of the URL
+			$urlSearch = '%'.$url.'%';
+		
+			// Create the prepare statement to search the table against the URL
+			$query = $this->dbh->prepare('SELECT * FROM metadata where url like :urlSearch');
+		
+			// Bind the variable to the above prepared statement
+			$query->bindParam(':urlSearch', $urlSearch, PDO::PARAM_STR);
+		
+			// Execute query and get results
+			$query->execute();
+			$result = $query->fetchAll(PDO::FETCH_ASSOC);
+			
+			return $result;
 		}
 	     
 
+		
 	    // Closing database connection  
 	    private static function closeConnection()
 	    {
 	    	return null;
 	    }
 }
+
 ?>
